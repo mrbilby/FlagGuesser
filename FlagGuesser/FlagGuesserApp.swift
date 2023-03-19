fcf363ad665bbc62e6d2bb4366211fd7a10099ee
mrbilby <baileymrb@gmail.com>
2023-03-19 09:12:52 +0100

    Initial Commit

---
+++ FlagGuesser/FlagGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuesserApp.swift
+//  FlagGuesser
+//
+//  Created by James Bailey on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
