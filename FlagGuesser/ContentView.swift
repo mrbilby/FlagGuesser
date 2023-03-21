df094237c0e08e9d4f48fd093232573ae7a87470
mrbilby <baileymrb@gmail.com>
2023-03-21 07:48:21 +0000

    First draft

--- FlagGuesser/ContentView.swift
+++ FlagGuesser/ContentView.swift
@@ -7,16 +7,114 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).capitalized + dropFirst()
     }
+
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
+struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var gameOver = false
+    @State private var scoreTitle = ""
+    @State private var scoreTotal = 0
+    @State private var gamePlayed = 0
+    @State private var countries = ["estonia", "france", "germany", "ireland", "italy", "nigeria", "poland", "russia", "spain", "uk", "us"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+
+    var body: some View {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of").font(.subheadline.weight(.heavy))
+                            .foregroundStyle(.secondary)
+                        Text(countries[correctAnswer].capitalizingFirstLetter()).font(.largeTitle.weight(.semibold))
+                    }
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original).clipShape(Capsule()).shadow(radius: 5)
+                            
+                            
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                Spacer()
+                Spacer()
+                Text("Score: \(scoreTotal)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Text("Games played: \(gamePlayed)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Spacer()
+            }
+            .padding()
+            
+        }
+        .alert(scoreTitle, isPresented: $gameOver) {
+            Button("Restart", action: resetGame)
+        } message: {
+            Text("Game Over score is \(scoreTotal) out of 8")
+        }
+
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(scoreTotal)")
+        }
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            scoreTotal = scoreTotal + 1
+            gamePlayed = gamePlayed + 1
+            if gamePlayed == 8 {
+                gameOver = true
+            }
+        } else {
+            scoreTitle = "Wrong, that is \(countries[number].capitalizingFirstLetter())"
+            gamePlayed = gamePlayed + 1
+            if gamePlayed == 8 {
+                gameOver = true
+            }
+
+        }
+
+        showingScore = true
+    }
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    func resetGame() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        scoreTotal = 0
+        gamePlayed = 0
+        showingScore = false
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
